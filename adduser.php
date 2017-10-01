c2cace5761113457c4e36f9086e670b58d35fa6e
Rushabh Modi <modi.rushabh179@gmail.com>
2017-10-01 15:06:34 +0530

    for adding users

---
+++ adduser.php
@@ -0,0 +1,17 @@
+<?php
+   $con=mysqli_connect("localhost","root","","dbpp");
+
+   if (mysqli_connect_errno($con)) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   }
+   
+   $name = $_POST['name'];
+   $id = $_POST['id'];
+   $password = $_POST['password'];
+   
+   mysqli_query($con,"INSERT INTO login (name,id,password) VALUES (\"$name\",\"$id\",\"$password\")");
+ 
+   echo "Done";
+      
+   mysqli_close($con);
+?>
