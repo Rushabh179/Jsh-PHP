8f068ab1f6bc652046911d19bd6373591e6d366f
Rushabh Modi <modi.rushabh179@gmail.com>
2017-10-01 23:03:28 +0530

    To fetch the list of rooms

---
+++ roomlist.php
@@ -0,0 +1,19 @@
+<?php
+   $con=mysqli_connect("localhost","root","","dbpp");
+
+   if (mysqli_connect_errno($con)) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   }
+	
+   $storeArray = Array();
+   $result = mysqli_query($con,"SELECT room_name FROM room");
+   while($row = mysqli_fetch_array($result)){
+	   $storeArray[] =  $row['room_name'];
+   }
+   
+   for($i=0;$i<count($storeArray);$i++){
+	echo $storeArray[$i]."  ";   
+   }
+   
+   mysqli_close($con);
+?>
